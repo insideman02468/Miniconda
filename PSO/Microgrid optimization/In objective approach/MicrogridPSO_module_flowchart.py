62048202be96e99c3831c06ed49a55173495a4d5
insideman02468 <insideman02468@gmail.com>
2019-06-21 17:47:47 +0900

    flowchartを修正し、確からしい挙動を確認しました。

--- PSO/Microgrid optimization/In objective approach/MicrogridPSO_module_flowchart.py
+++ PSO/Microgrid optimization/In objective approach/MicrogridPSO_module_flowchart.py
@@ -10,22 +10,26 @@ import numpy as np
 import pandas as pd
 
 
+# ? 太陽光の最大発電許容量PSO.pv_max(h)を求める関数
 def pv_generate(PSO):
     PSO.pv = (PSO.pv_cap_max / PSO.pv_capacity_per_unit) * \
         PSO.np_PV_efficient[PSO.h]
     return PSO.pv
 
 
+# ? 風力の最大発電許容量PSO.wind_max(h)を求める関数
 def wind_generate(PSO):
     PSO.wind = (PSO.wind_cap_max / PSO.wind_capacity_per_unit) * \
         PSO.np_Wind_efficient[PSO.h]
     return PSO.wind
 
 
+# ? タイムステップ1回分のフローチャート
 def flowchart(PSO):
 
     # * initialize some parameters
     if PSO.h == 0:
+        PSO.battery_need = 0
         PSO.p_battery = PSO.battery_cap_max * \
             PSO.initial_input_values["SOC_start[%]"]
         PSO.battery_max = PSO.battery_cap_max * \
@@ -40,75 +44,77 @@ def flowchart(PSO):
     PSO.battery_charging_power = 0
     PSO.battery_discharging_power = 0
     PSO.flowchart_root = str(PSO.h) + "h"
+    # ? SOCは、0 < SOC < 1 の範囲
+    SOC = (PSO.p_battery / PSO.battery_cap_max)
 
     # * 太陽光の発電量が需要より多いか確認
     if PSO.pv + PSO.wind > PSO.np_demand[PSO.h]:
-        # * 太陽光と風力の発電量が需要より多い時の処理
-        # TODO 改善箇所
-        # * バッテリーが過充電にならないかのチェック、過充電の場合、余った電気は捨てる。
         # * 風力発電が需要を上回るときの処理
-        SOC = (PSO.p_battery / PSO.battery_cap_max) * 100
-        battery_need = PSO.battery_cap_max * ((PSO.initial_input_values["SOC_start[%]"] - SOC) / 100)
+        # * ディーゼルは必要ない
+        PSO.Diesel_Cf = 0
+
+        # * バッテリーに必要な電力を導出
+        if SOC < PSO.initial_input_values["SOC_max[%]"]:
+            # * バッテリーの充電が必要
+            PSO.battery_need = PSO.battery_cap_max * \
+                (PSO.initial_input_values["SOC_max[%]"] - SOC)
+        else:
+            PSO.battery_need = 0
+            PSO.flowchart_root = str(
+                PSO.h) + "h: " + "Fullcharge ,no diesel "
+
         if PSO.wind > PSO.np_demand[PSO.h]:
-            # * バッテリのSOCを確認 スタート時よりより多いか確認
-            if SOC > PSO.initial_input_values["SOC_start[%]"]:
+            # * バッテリのSOCを確認 max時よりより多いか確認
+            if SOC > PSO.initial_input_values["SOC_max[%]"]:
                 # * バッテリーを充電する必要がない
                 PSO.wind = PSO.np_demand[PSO.h]
                 PSO.pv = 0
             else:
                 # * バッテリーを充電する必要がある
-                # * バッテリーを目標値まで充電可能か判断 風力が充電する電力 battery_need
-                if PSO.wind - PSO.np_demand[PSO.h] > battery_need:
-                    # * 目標SOCまで充電可能
-                    PSO.wind = PSO.np_demand[PSO.h] + battery_need
+                # * バッテリーを目標値まで充電可能か判断 風力が充電する電力 PSO.battery_need
+                if PSO.wind - PSO.np_demand[PSO.h] > PSO.battery_need:
+                    # * 目標SOCまで風力発電だけで充電可能
+                    PSO.wind = PSO.np_demand[PSO.h] + PSO.battery_need
                     PSO.pv = 0
+                    PSO.flowchart_root = str(
+                        PSO.h) + "h: " + "Battery charge only wind" + str(round(PSO.battery_need, 3))
                 else:
                     # * 目標SOCまで充電できないので、足りない分、太陽光が使えるかチェック
-                    battery_need_pv = PSO.wind - PSO.np_demand[PSO.h]
-                    if PSO.pv > battery_need - battery_need_pv:
+                    PSO.battery_need_pv = PSO.battery_need - \
+                        (PSO.wind - PSO.np_demand[PSO.h])
+                    if PSO.pv > PSO.battery_need_pv:
                         # * PVで賄える
-                        PSO.pv = battery_need - battery_need_pv
-                    # ! else
-                        # * PVで賄えない場合、ともに最大出力
+                        PSO.pv = PSO.battery_need_pv
+                        PSO.flowchart_root = str(
+                            PSO.h) + "h: " + "Battery charge wind and PV" + str(round(PSO.battery_need, 3))
+                    else:
+                        # * PVで賄えない場合、ともに最大出力であまりを充電
+                        PSO.battery_need = PSO.pv + \
+                            PSO.wind - PSO.np_demand[PSO.h]
+
         # * 風力発電が需要を上回らないときの処理、つまり、需要はカバーできるが、風力はフル出力でPVの発電量を調整
         else:
             # * バッテリーを充電する必要があるか確認
-            if SOC > PSO.initial_input_values["SOC_start[%]"]:
+            if SOC > PSO.initial_input_values["SOC_max[%]"]:
                 # * 充電する必要がなく、需要の不足分だけPVを発電する
                 PSO.pv = PSO.np_demand[PSO.h] - PSO.wind
             else:
                 # * 充電する必要があり、需要の不足分だけPVを発電した後、バッテリに充電する。
-                if PSO.pv - (PSO.np_demand[PSO.h] - PSO.wind) > battery_need:
+                if PSO.pv - (PSO.np_demand[PSO.h] -
+                             PSO.wind) > PSO.battery_need:
                     # * 目標SOCまで充電できる
-                    PSO.pv = PSO.np_demand[PSO.h] - PSO.wind + battery_need
-                # ! else:
+                    PSO.pv = PSO.np_demand[PSO.h] - PSO.wind + PSO.battery_need
+                else:
                     # * 目標SOCまで充電できない
+                    PSO.battery_need = PSO.pv + PSO.wind - PSO.np_demand[PSO.h]
 
-        if PSO.p_battery + (PSO.pv + PSO.wind -
-                            PSO.np_demand[PSO.h]) < PSO.battery_max:
-            PSO.p_battery = PSO.p_battery + \
-                (PSO.pv + PSO.wind - PSO.np_demand[PSO.h])
-            PSO.battery_charging_power = PSO.battery_charging_power + \
-                PSO.pv + PSO.wind - PSO.np_demand[PSO.h]
-            PSO.flowchart_root = str(
-                PSO.h) + "h: " + "Battery charge without diesel."
-            PSO.Diesel_Cf = 0
-        else:
-            PSO.trashed_power = (
-                (PSO.pv + PSO.wind - PSO.np_demand[PSO.h]) + PSO.p_battery) - PSO.battery_max
-            PSO.battery_charging_power = PSO.battery_charging_power + \
-                (PSO.battery_max - PSO.p_battery)
-            PSO.p_battery = PSO.battery_max
-            PSO.flowchart_root = str(
-                PSO.h) + "h: " + "Fullcharge ,no diesel, trashed " + str(
-                round(
-                    PSO.trashed_power,
-                    2)) + "[kWh]."
-            PSO.Diesel_Cf = 0
-    else:   # * 太陽光と風力の発電量が需要より少ない時の処理 = ディーゼルを使うかチェック
+    else:
+        # * 太陽光と風力の発電量が需要より少ない時の処理
+        # * ディーゼルを使うかチェック 当然、PSO.battery_need = 0
+        PSO.battery_need = 0
         # * ディーゼルを使用しない。
-        if PSO.p_battery > (PSO.np_demand[PSO.h] - PSO.pv - PSO.wind) and PSO.p_battery - (
-                PSO.np_demand[PSO.h] - PSO.pv - PSO.wind) > PSO.battery_min:
+        if PSO.p_battery - \
+                PSO.battery_min > (PSO.np_demand[PSO.h] - PSO.pv - PSO.wind):
             PSO.battery_discharging_power = PSO.np_demand[PSO.h] - \
                 PSO.pv - PSO.wind
             PSO.p_battery = PSO.p_battery - \
@@ -137,19 +143,23 @@ def flowchart(PSO):
                         PSO.p_diesel,
                         3)) + "[kWh]."
                 PSO.p_battery = PSO.battery_min
-                # TODO PSO.diesel_maxは大きすぎるため、ひとまず,rated output per
-                # unit=1.8に置き換え
+                """ TODO PSO.diesel_maxは大きすぎるため、ひとまず,rated output per unit=1.8に置き換え"""
                 PSO.Diesel_fc = PSO.initial_cost_parameters["Diesel_Adg"] * \
                     1.8 + PSO.initial_cost_parameters["Diesel_Bdg"] * PSO.p_diesel
                 PSO.Diesel_Cf = PSO.initial_cost_parameters["Diesel_Pf"] * \
                     PSO.Diesel_fc
+
+    # print(PSO.p_battery, PSO.p_battery + PSO.battery_need)
+    PSO.p_battery = PSO.p_battery + PSO.battery_need
+    PSO.battery_charging_power = PSO.battery_need
+    SOC = (PSO.p_battery / PSO.battery_cap_max)
 
     PSO.parameters = {"hour": PSO.h,
                       "pv": PSO.pv,
                       "pv+wind-demand": PSO.pv + PSO.wind - PSO.np_demand[PSO.h],
                       "wind": PSO.wind,
                       "battery state[kWh]": PSO.p_battery,
-                      "battery state[%]": (PSO.p_battery / PSO.battery_cap_max) * 100,
+                      "battery state[%]": SOC * 100,
                       "demand": PSO.np_demand[PSO.h],
                       "diesel power": PSO.p_diesel,
                       "trashed power": PSO.trashed_power,
@@ -162,25 +172,32 @@ def flowchart(PSO):
     return PSO.parameters, PSO.check, PSO.flowchart_root
 
 
-# * トータルコスト計算関数
+# ? トータルコスト計算関数
 def calc_cost(variables, initial_cost_parameters):
-    Et = [variables["pv_power_sum"] + variables["wind_power_sum"] +
-          variables["diesel_power_sum"] + variables["battery_discharging_power_sum"]] * 20
+    # * 年間総発電量を計算し、20列のベクトルにする
+    Et = [
+        variables["pv_power_sum"] + variables["wind_power_sum"] +
+        variables["diesel_power_sum"] + variables["battery_discharging_power_sum"]
+    ] * 20
     cost_parameters = {
         "(1+r)^t": (
             1 +
             np.array(
                 initial_cost_parameters["r[yen/year]"]) ** initial_cost_parameters["operation_year"]).tolist(),
     }
-    SCL = (((variables["pv_cap_max"] / 1000) * (np.array(initial_cost_parameters["It_PV_1kW[yen/year]"]) + np.array(initial_cost_parameters["Mt_PV_1kW[yen/year]"])))
-           + (variables["wind_cap_max"]
-              * (np.array(initial_cost_parameters["It_Wind_1kW[yen/year]"]) + np.array(initial_cost_parameters["Mt_Wind_1kW[yen/year]"])))
-           + (variables["diesel_max"]
-              * (np.array(initial_cost_parameters["It_Diesel_1kW[yen/year]"]) + np.array(initial_cost_parameters["Mt_Diesel_1kW[yen/year]"])))
-           + (variables["battery_cap_max"]
-              * (np.array(initial_cost_parameters["It_Battery_1kW[yen/year]"]) + np.array(initial_cost_parameters["Mt_Battery_1kW[yen/year]"])))
-           + np.array(variables["Diesel_Cf_sum"] * 20)
-           - np.array(variables["trashed_power_sum"] * 20) * np.array(initial_cost_parameters["Sell_income_from_trashed[kWh/yen]"])) \
+
+    # TODO O&Mの計算法を修正
+    SCL = (
+        ((variables["pv_cap_max"] / 1000) * (np.array(initial_cost_parameters["It_PV_1kW[yen/year]"]) + np.array(initial_cost_parameters["Mt_PV_1kW[yen/year]"])))
+        + (variables["wind_cap_max"]
+           * (np.array(initial_cost_parameters["It_Wind_1kW[yen/year]"]) + np.array(initial_cost_parameters["Mt_Wind_1kW[yen/year]"])))
+        + (variables["diesel_max"]
+           * (np.array(initial_cost_parameters["It_Diesel_1kW[yen/year]"]) + np.array(initial_cost_parameters["Mt_Diesel_1kW[yen/year]"])))
+        + (variables["battery_cap_max"]
+           * (np.array(initial_cost_parameters["It_Battery_1kW[yen/year]"]) + np.array(initial_cost_parameters["Mt_Battery_1kW[yen/year]"])))
+        + np.array(variables["Diesel_Cf_sum"] * 20)
+        - np.array(variables["trashed_power_sum"] * 20) * np.array(initial_cost_parameters["Sell_income_from_trashed[kWh/yen]"])
+    ) \
         / cost_parameters["(1+r)^t"]
 
     SEL = np.array(Et) / cost_parameters["(1+r)^t"]
